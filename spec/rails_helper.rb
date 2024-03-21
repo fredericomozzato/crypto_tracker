125d616b418ba5e839c9043e6d81598d8ffc4990
frederico.mozzato <frederico.smozzato@gmail.com>
2024-03-21 17:51:10 -0300

    Add factory bot

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,4 +41,5 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
   config.include Warden::Test::Helpers
+  config.include FactoryBot::Syntax::Methods
 end
