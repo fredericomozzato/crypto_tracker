ca4961ecda9a9babe5ae3be0045c6b2c6f8424c3
frederico.mozzato <frederico.smozzato@gmail.com>
2024-04-09 11:34:19 -0300

    Refactor buttons design

--- app/javascript/controllers/holdings_controller.js
+++ app/javascript/controllers/holdings_controller.js
@@ -4,16 +4,16 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["priceChange"];
   static values  = {
-    delta: Number
+    change: Number
   }
   
   connect() {
   }
   
-  priceChangeTargetConnected() {    
-    if (this.deltaValue > 0) {
+  priceChangeTargetConnected() {
+    if (this.changeValue > 0) {
       this.priceChangeTarget.className += " text-green-700";
-    } else if (this.deltaValue < 0) {
+    } else if (this.changeValue < 0) {
       this.priceChangeTarget.className += " text-red-700";
     } else {
       this.priceChangeTarget.className += " text-slate-500";
